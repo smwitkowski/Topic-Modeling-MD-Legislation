9bc36bc43524db3e36ad304683469ee47c02e0e8
smwitkowski <stephen.m.witkowski@gmail.com>
2018-10-15 19:48:43 -0400

    Defined items to scrape

--- legislation_scraper/legislation_scraper/items.py
+++ legislation_scraper/legislation_scraper/items.py
@@ -9,6 +9,13 @@ import scrapy
 
 
 class LegislationScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    #define the fields for your item here like:
+    bill_name = scrapy.Field()
+    bill_number = scrapy.Field()
+    url = scrapy.Field()
+    sponser = scrapy.Field()
+    status = scrapy.Field()
+    committee = scrapy.Field()
+    broad_subjects = scrapy.Field()
+    narrow_subjects = scrapy.Field()
     pass
