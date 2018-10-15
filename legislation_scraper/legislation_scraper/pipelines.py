6b870e1a851cce341a96e12a5006b3f0ff274c12
smwitkowski <stephen.m.witkowski@gmail.com>
2018-10-15 19:45:23 -0400

    Pipeline function to download PDFs

--- legislation_scraper/legislation_scraper/pipelines.py
+++ legislation_scraper/legislation_scraper/pipelines.py
@@ -4,8 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import requests
 
 class LegislationScraperPipeline(object):
+
     def process_item(self, item, spider):
+        url = item.get('pdf_url')
+        bill_number = item.get('bill_number')
+        save_loc = "C:\\Users\\switkowski\\Documents\\Projects\\Topic - Model - MD - Legislation\\data\\"
+        r = requests.get(url, stream = True)
+        with open(save_loc + bill_number + '.pdf', 'wb') as f:
+            f.write(r.conetnt)
         return item
--- legislation_scraper/legislation_scraper/settings.py
+++ legislation_scraper/legislation_scraper/settings.py
@@ -64,17 +64,17 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'legislation_scraper.pipelines.LegislationScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'legislation_scraper.pipelines.bill_download': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
