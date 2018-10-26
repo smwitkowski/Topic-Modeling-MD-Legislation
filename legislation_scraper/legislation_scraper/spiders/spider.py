dbcead347ea5852d5aa1e1a77724da244af3d238
smwitkowski <stephen.m.witkowski@gmail.com>
2018-10-25 20:23:41 -0400

    Resoled pipeline reference

--- legislation_scraper/legislation_scraper/spiders/spider.py
+++ legislation_scraper/legislation_scraper/spiders/spider.py
@@ -11,18 +11,18 @@ class LegislationSpider(CrawlSpider):
     allowed_domains = ['mgaleg.maryland.gov']
     start_urls = [
         'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=app&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=b%26t&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=ecm&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=ehe&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=env&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=exn&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=fin&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=hgo&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=jpr&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=jud&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=sru&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=hru&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
-        #'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=w%26m&stab=02&pid=cmtepage&tab=subject3&ys=2018RS'
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=b%26t&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=ecm&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=ehe&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=env&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=exn&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=fin&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=hgo&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=jpr&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=jud&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=sru&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=hru&stab=02&pid=cmtepage&tab=subject3&ys=2018RS',
+        'http://mgaleg.maryland.gov/webmga/frmMain.aspx?id=w%26m&stab=02&pid=cmtepage&tab=subject3&ys=2018RS'
     ]
 
     rules = [
